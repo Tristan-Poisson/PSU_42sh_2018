03b4066b494b9f124574476cd255d5f75a103f4c
Dorian <dorian.collin@epitech.eu>
2019-05-13 12:54:06 +0200

    add prototypes.h and main.c

---
+++ include/prototypes.h
@@ -0,0 +1,7 @@
+/*
+** EPITECH PROJECT, 2019
+** 42sh
+** File description:
+** File with prototypes
+*/
+
---
+++ src/main.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2019
+** 42sh
+** File description:
+** Main file
+*/
+
+int main(void)
+{
+    return (0);
+}
