f19f8f97f0adfbc0b0aba71f4d4e61a486569398
Nolan <nolan.kijas@epitech.eu>
2019-05-25 11:27:42 +0000

    fix built-in exit

--- src/built_in_exit.c
+++ src/built_in_exit.c
@@ -10,18 +10,33 @@
 #include <string.h>
 #include <stdlib.h>
 
+void exit_error(void)
+{
+    write(2, "exit: Expression Syntax.\n", 25);
+}
+
+int check_number(char **com)
+{
+    for (int j = 0; com[1][j] != '\0'; j++)
+        if (com[1][j] < 48 || com[1][j] > 57) {
+            exit_error();
+            return (-1);
+        }
+    return (0);
+}
+
 int my_exit(char **command)
 {
     if (command[2]) {
-        my_perror(command[0]);
+        exit_error();
         return (84);
     }
     if (command[1] == NULL) {
         printf("exit\n");
         exit(0);
     }
-    if (my_atoi(command[0]) != -1)
-        exit(my_atoi(command[0]));
+    if (check_number(command) != -1)
+        exit(atoi(command[1]));
     else
-        return (0);
+        return (84);
 }
