037dfdec7b67ba2650337112301b952ce2df8308
Nolan <nolan.kijas@epitech.eu>
2019-05-26 16:27:27 +0000

    fix env built-in

--- src/built_in_env.c
+++ src/built_in_env.c
@@ -9,11 +9,39 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <errno.h>
+
+int letter(char c, int low, int up)
+{
+    if (c > low && c < up)
+        return (1);
+    else
+        return (0);
+}
+
+int catch_error(char **command)
+{
+    if (!letter(command[1][0], 65, 90) && !letter(command[1][0], 97, 122)) {
+        printf("setenv: Variable name must begin with a letter.\n");
+        return (-1);
+    }
+    return (0);
+}
 
 int my_setenv(char **command)
 {
     char *arg;
+    int args = 0;
 
+    for (int i = 0; command[i]; i++)
+        args++;
+    if (args > 3) {
+        printf("setenv: Too many arguments.\n");
+        return (84);
+    }
+    if (catch_error(command) == -1) {
+        return (84);
+    }
     if (command[2] == NULL)
         arg = "";
     else
